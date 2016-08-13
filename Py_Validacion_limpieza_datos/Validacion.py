4b957a7fee3cb8c473f08dfe6860e19fbb874190
dsc <carloshp11@gmail.com>
2016-08-13 19:49:21 +0200

    Building up Validacion.py

--- Py_Validacion_limpieza_datos/Validacion.py
+++ Py_Validacion_limpieza_datos/Validacion.py
@@ -1,24 +1,41 @@
 import os
 import re
 
-# -------------------------------------------------/ Fixed variables developement /-----------------------------------------
+# ---------------------------------------/ Fixed variables developement /-----------------------------------------
 
 Project_name = 'Project_20160811_001'
 date = '20160811'
 
-# -------------------------------------------------/ Variable definitions  /----------------------------------------
+# -----------------------------------------/ Variable definitions  /----------------------------------------------
 
 path_Definitions = os.path.join(os.getcwd(), Project_name, 'Definitions')
-path_Input = os.path.join(os.getcwd(), Project_name, 'Definitions')
-path_Output = os.path.join(os.getcwd(), Project_name, 'Definitions')
-path_Filtered = os.path.join(os.getcwd(), Project_name, 'Definitions')
+path_Input = os.path.join(os.getcwd(), Project_name, 'Input')
+path_Output = os.path.join(os.getcwd(), Project_name, 'Output')
+path_Filtered = os.path.join(os.getcwd(), Project_name, 'Filtered_out')
 
-# -------------------------------------------------/ Class definitions  /----------------------------------------
+# -----------------------------------------/ function definitions  /----------------------------------------------
+
+
+def detect_delimiter(sample_line):
+    delimiter = None
+    greater = 1
+    if len(sample_line.split('\t')) > greater:
+        delimiter = '\t'
+    if len(sample_line.split(';')) > greater:
+        delimiter = ';'
+    if len(sample_line.split(',')) > greater:
+        delimiter = ','
+    if delimiter is None:
+        raise SyntaxError('Delimiter wasn\'t detected as one of these values: \\t, ; or ,')
+
+# ------------------------------------------/ Class definitions  /------------------------------------------------
 
 
 class File(object):
-    def __init__(self, path, input_mode = True):
+    def __init__(self, path, input_mode=True):
         if input_mode:
+            self.handler = open(path, 'r', encoding="utf-8")
+            self.first_line = self.handler.readline()
             self.handler = open(path, 'r', encoding="utf-8")
         else:
             self.handler = open(path, 'w', encoding="utf-8")
@@ -26,8 +43,8 @@ class File(object):
 
 
 class DefFile(File):
-    def __init__(self, path, input_mode = True):
-        File.__init__(self, path, input_mode)
+    def __init__(self, path):
+        File.__init__(self, path, True)
         self.columns = {}
         self.delimiter = '\t'
 
@@ -39,9 +56,15 @@ class DefFile(File):
             line = line.split(self.delimiter)
             if len(line) != 3:
                 raise ValueError('%s delimiter file has incorrect number of columns' % self.name)
-            if line[1] not in ['character', 'integer', 'numeric', 'bool']:
+            if line[1] not in ['character', 'integer', 'numeric', 'bool', 'date']:
                 raise ValueError('Invalid type for column %s in %s file' % (line[0], self.name))
             self.columns[line[0]] = line[2]
+
+
+class InputFile(File):
+    def __init__(self, path):
+        File.__init__(self, path, True)
+        self.delimiter = detect_delimiter(self.first_line)
 
 
 # -------------------------------------------------/ Definitions /-----------------------------------------
@@ -60,9 +83,13 @@ if len(definitions) == 0:
 if len(os.listdir(path_Input)) == 0 or len(os.listdir(path_Input)) != len(definitions):
     raise FileNotFoundError('Input files missing or more than definitions')
 
-Input = []
+Input = {}
 for file in os.listdir(path_Input):
-    Input.append(File(os.path.join(path_Input, file)))
+    if not file.endswith('.csv'):
+        continue
+    element = InputFile(os.path.join(path_Input, file))
+    Input[element.name] = element
+element = None
 
 
 
